c75181b028ddff9ce5eaeb826b529d7a0063803d
= <it7mdx@googlemail.com>
2021-12-03 14:17:01 +0300

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="text">{{ msg }}</h1>
+    <h3>
+      You’ve successfully created a project with
+      <a target="_blank" href="https://vitejs.dev/">Vite</a> +
+      <a target="_blank" href="https://v3.vuejs.org/">Vue 3</a>.
+    </h3>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.text {
+@extend .text-danger
+}
+</style>
